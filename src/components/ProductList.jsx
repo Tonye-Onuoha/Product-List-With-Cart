5b099d6f4b64bc5dcfca0b76004233bd90bbd22c
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-06-19 22:27:11 +0100

    Created "ProductList" component

---
+++ src/components/ProductList.jsx
@@ -0,0 +1,11 @@
+import Desserts from "./Desserts";
+import Cart from "./Cart";
+
+export default function ProductList() {
+    return (
+        <div className="product-list">
+            <Desserts />
+            <Cart />
+        </div>
+    );
+}
