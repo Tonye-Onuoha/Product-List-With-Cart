693573396ce32bdb8874338ad2e939e580083fdf
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-06-23 02:26:04 +0100

    Created "OrderConfirmed" component

---
+++ src/components/OrderConfirmed.jsx
@@ -0,0 +1,34 @@
+import confirmedIcon from "../assets/images/icon-order-confirmed.svg";
+import ConfirmedItem from "./ConfirmedItem";
+
+export default function OrderConfirmed({ orders, orderTotal, onNewOrder }) {
+    return (
+        <div
+            className={
+                window.innerWidth >= 1200
+                    ? "order-confirmed-container order-confirmed-container--desktop"
+                    : "order-confirmed-container"
+            }>
+            <img src={confirmedIcon}></img>
+            <h1 className="order-confirmed__heading">Order Confirmed</h1>
+            <p className="order-confirmed__text">We hope you enjoy your food!</p>
+            <div className="confirmed-orders">
+                {orders.map((order) => (
+                    <ConfirmedItem key={order.id} item={order} />
+                ))}
+                <div className="order-total__text">
+                    <p className="order-total__label">Order Total</p>
+                    <h1 className="order-total__cost">${orderTotal.toFixed(2)}</h1>
+                </div>
+            </div>
+            <button
+                type="button"
+                className="new-order-button"
+                onClick={onNewOrder}
+                onMouseEnter={(e) => (e.currentTarget.style.cursor = "pointer")}
+                onMouseLeave={(e) => (e.currentTarget.style.cursor = "default")}>
+                Start New Order
+            </button>
+        </div>
+    );
+}
