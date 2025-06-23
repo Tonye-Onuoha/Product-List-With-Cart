727b99e0225d4507ecd51c6c9c3f29b46b2067e2
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-06-23 02:55:37 +0100

    Updated "DessertButton" functionality

--- src/components/DessertButton.jsx
+++ src/components/DessertButton.jsx
@@ -1,9 +1,51 @@
+import { useState, useContext, useEffect } from "react";
+import { OrdersContext } from "../Context";
 import AddButton from "./buttons/AddButton";
 import OrderButton from "./buttons/OrderButton";
 
-export default function DessertButton({ isSelected }) {
-    if (isSelected) {
-        return <OrderButton />;
+export default function DessertButton({ category, isSelected }) {
+    const [itemCount, setItemCount] = useState(1);
+    const { orders, UpdateOrders } = useContext(OrdersContext);
+
+    function handleDecrementClick() {
+        if (itemCount === 1) return;
+        setItemCount(itemCount - 1);
+        const updatedOrders = orders.map((order) => {
+            if (order.category === category) {
+                const itemPrice = (itemCount - 1) * order.price;
+                const updatedOrder = { ...order, quantity: itemCount - 1, cost: itemPrice };
+                return updatedOrder;
+            } else return order;
+        });
+        UpdateOrders(updatedOrders);
     }
-    return <AddButton />;
+
+    function handleIncrementClick() {
+        setItemCount(itemCount + 1);
+        const updatedOrders = orders.map((order) => {
+            if (order.category === category) {
+                const itemPrice = (itemCount + 1) * order.price;
+                const updatedOrder = { ...order, quantity: itemCount + 1, cost: itemPrice };
+                return updatedOrder;
+            } else return order;
+        });
+        UpdateOrders(updatedOrders);
+    }
+
+    useEffect(() => {
+        if (orders.length === 0 && itemCount !== 1) {
+            setItemCount(1);
+        }
+    });
+
+    if (isSelected) {
+        return (
+            <OrderButton
+                itemCount={itemCount}
+                onDecrementClick={handleDecrementClick}
+                onIncrementClick={handleIncrementClick}
+            />
+        );
+    }
+    return <AddButton itemCount={itemCount} />;
 }
