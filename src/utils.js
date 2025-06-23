a8c332cabadf8f6ca941c56769b091ea5d3abf63
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-06-23 02:51:43 +0100

    Added "confirmedOrderClass" function to utils module

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,19 @@
-export const classNameSetter = (value) => {
+export const dessertImageClass = (value) => {
     if (value.includes(" ")) {
         const [first, last] = value.split(" ");
         return "dessert-image " + `${first}-${last}`.toLowerCase();
     }
     return "dessert-image " + value.toLowerCase();
 };
+
+export const confirmedOrderClass = (value) => {
+    if (value.includes(" ")) {
+        const [first, last] = value.split(" ");
+        return `${first}-${last}`.toLowerCase() + "-thumbnail";
+    }
+    return value.toLowerCase() + "-thumbnail";
+};
+
+let orderID = 0;
+
+export const orderIdGenerator = () => ++orderID;
