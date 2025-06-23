56c851a0b1565b1a6d7c6d001b3dd52e0751a890
Tonye Hugo Onuoha <tonyeonuoha@gmail.com>
2025-06-23 04:06:42 +0100

    Added loading and error state to "DessertList" component

--- src/components/Desserts.jsx
+++ src/components/Desserts.jsx
@@ -3,20 +3,56 @@ import DessertsList from "./DessertsList";
 
 export default function Desserts() {
     const [desserts, setDesserts] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
 
     useEffect(() => {
         let ignore = false;
         async function fetchDesserts() {
-            const response = await fetch("http://localhost:5173/data.json");
-            const data = await response.json();
-            if (!ignore) setDesserts(data);
+            try {
+                const response = await fetch("http://localhost:5173/data.json");
+                const data = await response.json();
+                if (!ignore) {
+                    setDesserts(data);
+                    setLoading(false);
+                }
+            } catch (err) {
+                setError("Sorry, unable to fetch desserts at the moment.");
+                setLoading(false);
+            }
         }
         fetchDesserts();
         return () => (ignore = true);
     }, []);
 
-    if (desserts.length === 0) {
-        return <p>No available desserts!</p>;
+    if (desserts.length === 0 && loading) {
+        return (
+            <div
+                className="desserts-container"
+                style={{
+                    paddingTop: "50px",
+                    paddingBottom: "50px",
+                    backgroundColor: "lightgray",
+                    borderRadius: "10px"
+                }}>
+                <h2 className="loading-text">Please wait, loading available desserts...</h2>
+            </div>
+        );
+    }
+
+    if (desserts.length === 0 && error) {
+        return (
+            <div
+                className="desserts-container"
+                style={{
+                    paddingTop: "50px",
+                    paddingBottom: "50px",
+                    backgroundColor: "lightgray",
+                    borderRadius: "10px"
+                }}>
+                <h2 className="loading-text">{error}</h2>
+            </div>
+        );
     }
 
     return (
